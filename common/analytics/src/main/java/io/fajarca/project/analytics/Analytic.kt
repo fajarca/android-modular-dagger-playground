f844f2377708a3f48d12aae6a0844be228fa0f0d
Fajar Chaeril Azhar <chaeril.fajar@gmail.com>
2021-07-10 17:34:39 +0700

    Send analytics

--- common/analytics/src/main/java/io/fajarca/project/analytics/Analytic.kt
+++ common/analytics/src/main/java/io/fajarca/project/analytics/Analytic.kt
@@ -1,5 +1,8 @@
 package io.fajarca.project.analytics
 
+import io.fajarca.project.analytics.data.response.LogEventDto
+import io.fajarca.project.apiclient.response.ApiResponse
+
 interface Analytic {
-    suspend fun logEvent(key: String, value: Any): Boolean
+    suspend fun logEvent(key: String, value: Any): ApiResponse<Exception, LogEventDto>
 }
--- common/analytics/src/main/java/io/fajarca/project/analytics/AnalyticImpl.kt
+++ common/analytics/src/main/java/io/fajarca/project/analytics/AnalyticImpl.kt
@@ -1,19 +1,21 @@
 package io.fajarca.project.analytics
 
+import io.fajarca.project.analytics.data.request.LogEventRequest
+import io.fajarca.project.analytics.data.response.LogEventDto
 import io.fajarca.project.analytics.data.service.AnalyticService
+import io.fajarca.project.apiclient.ApiClient
+import io.fajarca.project.apiclient.response.ApiResponse
 import javax.inject.Inject
 
 
 class AnalyticImpl @Inject constructor(
-    private val analyticService: AnalyticService
+    private val analyticService: AnalyticService,
+    private val apiClient: ApiClient
 ) : Analytic {
 
-    override suspend fun logEvent(key: String, value: Any): Boolean {
-    /*    val request = LogEventRequest(key, value.toString())
-        return apiClient.call { analyticService.logEvent(request) }*/
-
-
-        return true
+    override suspend fun logEvent(key: String, value: Any): ApiResponse<Exception, LogEventDto> {
+        val request = LogEventRequest(key, value.toString())
+        return apiClient.call { analyticService.logEvent(request) }
     }
 
 }
