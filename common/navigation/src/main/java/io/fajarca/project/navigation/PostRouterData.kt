5da7346c203373a1d3761630e2e4539dc9e96166
Fajar Chaeril Azhar <chaeril.fajar@gmail.com>
2021-07-10 18:54:17 +0700

    Fix deprecated issue

--- common/navigation/src/main/java/io/fajarca/project/navigation/PostRouterData.kt
+++ common/navigation/src/main/java/io/fajarca/project/navigation/PostRouterData.kt
@@ -1,7 +1,7 @@
 package io.fajarca.project.navigation
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class PostRouterData(val postId: Int) : Parcelable
--- common/navigation/src/main/java/io/fajarca/project/navigation/UserRouterData.kt
+++ common/navigation/src/main/java/io/fajarca/project/navigation/UserRouterData.kt
@@ -1,7 +1,7 @@
 package io.fajarca.project.navigation
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class UserRouterData(val userId: Int) : Parcelable
