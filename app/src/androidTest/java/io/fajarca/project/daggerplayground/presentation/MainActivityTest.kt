19c0d13b772ebdab793fa63c1e92d61c5fa69c86
Fajar Chaeril Azhar <chaeril.fajar@gmail.com>
2021-07-13 21:41:38 +0700

    Add ui test dependencies

---
+++ app/src/androidTest/java/io/fajarca/project/daggerplayground/presentation/MainActivityTest.kt
@@ -0,0 +1,22 @@
+package io.fajarca.project.daggerplayground.presentation
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MainActivityTest {
+
+    @get:Rule
+    var activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun launchMainActivity() {
+       // val scenario = activityRule.scenario
+    }
+
+}
