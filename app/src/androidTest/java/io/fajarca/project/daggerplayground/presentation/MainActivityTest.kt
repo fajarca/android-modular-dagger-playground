4071ff3982730acfb14404a87bb60ee6983d306b
Fajar Chaeril Azhar <chaeril.fajar@gmail.com>
2021-07-13 22:06:36 +0700

    Add ui test for main activity

--- app/src/androidTest/java/io/fajarca/project/daggerplayground/presentation/MainActivityTest.kt
+++ app/src/androidTest/java/io/fajarca/project/daggerplayground/presentation/MainActivityTest.kt
@@ -1,8 +1,13 @@
 package io.fajarca.project.daggerplayground.presentation
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import io.fajarca.project.daggerplayground.R
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,8 +20,20 @@ class MainActivityTest {
     var activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun launchMainActivity() {
-       // val scenario = activityRule.scenario
+    fun whenLaunched_buttonMovieModuleShouldDisplayed() {
+        onView(withId(R.id.btnGoToMovieModule))
+            .check(matches(isDisplayed()))
     }
 
+    @Test
+    fun whenLaunched_buttonPostModuleShouldDisplayed() {
+        onView(withId(R.id.btnGoToPostModule))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun whenLaunched_buttonUserModuleShouldDisplayed() {
+        onView(withId(R.id.btnGoToUserModule))
+            .check(matches(isDisplayed()))
+    }
 }
